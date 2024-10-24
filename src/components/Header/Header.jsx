4b061ac937a782e87c26660fb2c8e712cfe0d908
Ren <reine.donnestad@gmail.com>
2024-10-23 20:27:01 -0400

    added feature/header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.scss";
+
+function Header (){
+    return(
+    <img className="header__logo" src="src/assets/images/Logo/BrainFlix-logo.svg" alt="brainflix__logo"></img>
+);
+}
+
+export default Header;
+
